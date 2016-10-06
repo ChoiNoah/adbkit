da9f247b098c6c1ab5c878b89fa60a03316caa8c
Noah Choi <noah@protopie.io>
2016-10-06 18:06:47 +0900

    make index.js depends only to js code

--- index.js
+++ index.js
@@ -4,12 +4,7 @@
   Path = require('path');
 
   module.exports = (function() {
-    switch (Path.extname(__filename)) {
-      case '.coffee':
-        return require('./src/adb');
-      default:
         return require('./lib/adb');
-    }
   })();
 
 }).call(this);
